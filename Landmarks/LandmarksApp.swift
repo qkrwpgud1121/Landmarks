de9b16d19c615155e50fef8a1cee2cdbfb143ca2
jahyeang <qkrwpgud1121@gmail.com>
2024-10-31 17:34:08 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 박제형 on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
