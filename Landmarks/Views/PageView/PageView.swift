71817f67fe49f77079052e28c08fc96996b83d23
jahyeang <qkrwpgud1121@gmail.com>
2024-11-06 17:19:51 +0900

    Coordinator

---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,22 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by 박제형 on 11/6/24.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
