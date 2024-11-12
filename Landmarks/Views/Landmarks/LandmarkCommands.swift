4653bd8df13987bd45ae6b63c04584038c51f959
jahyeang <qkrwpgud1121@gmail.com>
2024-11-12 17:46:10 +0900

    macOS Setting, Option

---
+++ Landmarks/Views/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,36 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by 박제형 on 11/12/24.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
+    var body: some Commands {
+        SidebarCommands()
+        
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
+    }
+}
